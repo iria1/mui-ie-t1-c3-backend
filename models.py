978ef88dd847bf1f7e40fa9789ded9467bc56aee
Irfandi Riawan <irfandi.riawan@is-gs.com>
2025-06-20 11:04:25 +0700

    Add SocmedMental and update BullyStatRegional columns

--- models.py
+++ models.py
@@ -15,9 +15,23 @@ class BullyStatRegional(db.Model):
     __tablename__ = 'data_bully_stat_regional'
 
     id = db.Column(db.Integer, primary_key=True)
-    region = db.Column(db.String(100))
+    country = db.Column(db.String(100))
     total_pct = db.Column(db.Integer)
     male_pct = db.Column(db.Integer)
     female_pct = db.Column(db.Integer)
     source = db.Column(db.String(100))
+    region = db.Column(db.String(100))
+    active = db.Column(db.Boolean, nullable=False, default=True, server_default=str('1'))
+
+class SocmedMental(db.Model):
+    __tablename__ = 'data_socmed_mental'
+
+    id = db.Column(db.Integer, primary_key=True)
+    age = db.Column(db.Integer)
+    gender = db.Column(db.String(10))
+    acad_level = db.Column(db.String(25))
+    country = db.Column(db.String(50))
+    daily_socmed_usage_hrs = db.Column(db.Numeric(10,2))
+    daily_sleep_hrs = db.Column(db.Numeric(10,2))
+    mental_health_score = db.Column(db.Integer)
     active = db.Column(db.Boolean, nullable=False, default=True, server_default=str('1'))
