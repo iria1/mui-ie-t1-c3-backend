3b7b49b16822129f1d7c7988fd1d59bccdcc37a9
Irfandi Riawan <irfandi.riawan@is-gs.com>
2025-08-25 16:48:23 +0700

    Add endpoint for message analyzer

---
+++ routes/message_analyzer_v1.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, request, jsonify
+from werkzeug.exceptions import BadRequest
+from utils.utility import validate_request_params, sanitize_string
+from utils.jwt import require_token
+
+message_analyzer_v1_bp = Blueprint('message_analyzer_v1_bp', __name__, url_prefix='/v1/message_analyzer')
+
+@message_analyzer_v1_bp.route('/foo', methods=['POST'])
+@require_token
+def get_response_from_chatbot():
+    if not request.is_json:
+        raise BadRequest('Request must be JSON')
+    
+    is_valid, error_response = validate_request_params(request, ['message'])
+    if not is_valid:
+        raise BadRequest(error_response)
+
+    data = request.get_json()
+    message = sanitize_string(data['message'])
+    
+    return jsonify({
+        'data': {
+            'message': message
+        }
+    }), 200
